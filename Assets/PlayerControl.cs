489b7e378b93197d8df93d684b4334aa7fab0e25
delevinying <846353427@qq.com>
2017-02-17 12:51:52 +0800

    简单Demo_part1

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			gameObject.transform.position += new Vector3 (0.1f, 0, 0);
+		}else if (Input.GetKey (KeyCode.UpArrow)) {
+			gameObject.transform.position += new Vector3 (0, 0.1f, 0);
+		}else if (Input.GetKey (KeyCode.LeftArrow)) {
+			gameObject.transform.position += new Vector3 (-0.1f, 0, 0);
+		}
+	}
+}
